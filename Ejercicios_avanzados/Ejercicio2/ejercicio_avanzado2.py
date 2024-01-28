30e2283fb26301b0746388a0235bc2749c2ad462
emiezeoc <emiezeoc@gmail.com>
2024-01-28 18:58:36 -0300

    Update ejercicio_avanzado2.py

--- Ejercicios_avanzados/Ejercicio2/ejercicio_avanzado2.py
+++ Ejercicios_avanzados/Ejercicio2/ejercicio_avanzado2.py
@@ -11,12 +11,15 @@ def armar_equipo():
         puntaje_equipo = int(input("Ingrese el puntaje del equipo: "))
         goles_a_favor = int(input("Ingrese la cantidad de goles a favor: "))
         lista_equipos.append([nombre_equipo,puntaje_equipo,goles_a_favor])
-        nombre_equipo = input("Ingrese el nombre del equipo(coloque fin para terminar el programa): ").upper()
+        nombre_equipo = input("Ingrese el nombre del equipo(coloque ZZZ para terminar el programa): ").upper()
     return lista_equipos
 
-def imprimir_goles_primero_ultimo():
-    return 
+def imprimir_goles_primero_ultimo(lista):
+    Goles_primer_equipo = lista[0][2]
+    goles_ultimo_equipo = lista[-1][2]
+    return print("Los goles del primer equipo son: " + str(Goles_primer_equipo) + ", los goles del ultimo equipo son: " + str(goles_ultimo_equipo))
 
 #Codigo principal
 
-print(armar_equipo())
+lista_equipos = armar_equipo()
+imprimir_goles_primero_ultimo(lista_equipos)
